41e9a4264949bc23339348475102802f9a2f6261
dvoroniuc <denis.voroniuc@isa.utm.md>
2021-03-12 00:03:18 +0200

    added second lab

--- App.tsx
+++ App.tsx
@@ -1,3 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
 import { Routes } from "./src/Routes";
 
-export default Routes;
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Routes colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
